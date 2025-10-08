e763ff641f84596112adb9ced0078ef3ab4c044b
iHussamAlshayeb <hussamalshayeb01@gmail.com>
2025-10-08 19:58:00 +0300

    Update user.routes.js

--- server/src/api/user.routes.js
+++ server/src/api/user.routes.js
@@ -1,16 +1,22 @@
-import express from 'express';
-import { getMyProfile, getMyArtworks } from './user.controller.js';
-import { protect } from '../middleware/auth.middleware.js'; // Import the middleware
+import express from "express";
+import {
+  getMyProfile,
+  getMyArtworks,
+  getMyActiveBids,
+  getMyWonArtworks,
+} from "./user.controller.js";
+import { protect } from "../middleware/auth.middleware.js"; // Import the middleware
 
 const router = express.Router();
 
 // Any request to this route must first pass through the 'protect' middleware
-router.get('/profile', protect, getMyProfile);
+router.get("/profile", protect, getMyProfile);
 
 // Add the new route for fetching a student's own artworks
-router.get('/me/artworks', protect, getMyArtworks);
+router.get("/me/artworks", protect, getMyArtworks);
 
-router.get('/me/bids', protect, getMyActiveBids);
-router.get('/me/wins', protect, getMyWonArtworks);
+// المسارات الجديدة
+router.get("/me/bids", protect, getMyActiveBids);
+router.get("/me/wins", protect, getMyWonArtworks);
 
 export default router;
