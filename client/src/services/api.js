d550e7edca6f7013fa9146ae5fd314cf767b5aa4
iHussamAlshayeb <hussamalshayeb01@gmail.com>
2025-10-13 17:14:04 +0300

    Custom Domain

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const apiClient = axios.create({
-  baseURL: 'https://art-auction-api-0kqg.onrender.com/api/v1',
+  baseURL: 'https://api.fanan3.com/api/v1',
   headers: {
     'Content-Type': 'application/json',
   },
