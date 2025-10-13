6f942291cee99a02363b2edfca5c2c30ab68c9f7
iHussamAlshayeb <hussamalshayeb01@gmail.com>
2025-10-13 20:47:22 +0300

    Update admin.routes.js

--- server/src/api/admin.routes.js
+++ server/src/api/admin.routes.js
@@ -1,19 +1,22 @@
-import express from 'express';
-import { getStats, getAllUsers } from './admin.controller.js';
-import { protect, checkRole } from '../middleware/auth.middleware.js';
+import express from "express";
+import {
+  getStats,
+  getAllUsers,
+  updateUserRole,
+  deleteUser,
+} from "./admin.controller.js";
+import { protect, checkRole } from "../middleware/auth.middleware.js";
 
 const router = express.Router();
 
 // حماية كل المسارات في هذا الملف: يجب أن يكون المستخدم مسجلاً دخوله وأن يكون دوره 'ADMIN'
-router.use(protect, checkRole(['ADMIN']));
+router.use(protect, checkRole(["ADMIN"]));
 
 // تعريف المسارات
-router.get('/stats', getStats);
-router.get('/users', getAllUsers);
+router.get("/stats", getStats);
+router.get("/users", getAllUsers);
 
-
-router.put('/users/:id/role', updateUserRole);
-router.delete('/users/:id', deleteUser);
-
+router.put("/users/:id/role", updateUserRole);
+router.delete("/users/:id", deleteUser);
 
 export default router;
