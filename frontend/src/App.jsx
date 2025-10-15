a8e1eabd8de5bd77f4329df8661b8437e28d817c
iHussamAlshayeb <hussamalshayeb01@gmail.com>
2025-10-15 13:56:34 +0300

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -47,7 +47,10 @@ function App() {
 
           {/* الروابط */}
           <div className="flex items-center gap-6">
-            {/* --== تم إصلاح الخطأ المنطقي هنا ==-- */}
+            <Link
+              to="/gallary"
+              className="text-gray-700 hover:text-orange-600 transition-colors font-medium"
+            ></Link>
             {user ? (
               // حالة المستخدم المسجل دخوله
               <>
