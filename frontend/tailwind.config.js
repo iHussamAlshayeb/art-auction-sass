16509952b7f888f25fe4c5076d6edf9374f1d65e
iHussamAlshayeb <hussamalshayeb01@gmail.com>
2025-10-19 17:58:57 +0300

    new logo and colors

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -4,7 +4,36 @@ export default {
   theme: {
     extend: {
       fontFamily: {
-        sans: ["Tajawal", "sans-serif"],
+        sans: ["Cairo", "sans-serif"],
+        heading: ["Tajawal", "sans-serif"],
+      },
+      primary: {
+        // اللون الفيروزي
+        DEFAULT: "#4DB6AC", // لون فيروزي متوسط
+        50: "#E0F2F1",
+        100: "#B2DFDB",
+        200: "#80CBC4",
+        300: "#4DB6AC",
+        400: "#26A69A",
+        500: "#009688",
+        600: "#00897B",
+        700: "#00796B",
+        800: "#00695C",
+        900: "#004D40",
+      },
+      secondary: {
+        // اللون البرتقالي المحمر
+        DEFAULT: "#FF6F61", // لون برتقالي محمر متوسط
+        50: "#FFEBEE",
+        100: "#FFCDD2",
+        200: "#EF9A9A",
+        300: "#E57373",
+        400: "#EF5350",
+        500: "#F44336",
+        600: "#E53935",
+        700: "#D32F2F",
+        800: "#C62828",
+        900: "#B71C1C",
       },
     },
   },
