e07a9904db178d03804e691aa19685fc5805993a
iHussamAlshayeb <hussamalshayeb01@gmail.com>
2025-10-20 00:44:19 +0300

    Update LogoutButton.jsx

--- frontend/src/components/LogoutButton.jsx
+++ frontend/src/components/LogoutButton.jsx
@@ -1,5 +1,5 @@
 import { useNavigate } from 'react-router-dom';
-import { useAuth } from '../context/AuthContext';
+import { useAuth } from '../context/AuthContext.jsx';
 import { LogOut } from 'lucide-react';
 
 function LogoutButton() {
@@ -23,3 +23,4 @@ function LogoutButton() {
 }
 
 export default LogoutButton;
+
