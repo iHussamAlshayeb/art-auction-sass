e67b2b95c83da03ef94743b83f00ede6704fa41b
iHussamAlshayeb <hussamalshayeb01@gmail.com>
2025-10-20 13:00:28 +0300

    Update auction.controller.js

--- backend/src/api/auction.controller.js
+++ backend/src/api/auction.controller.js
@@ -31,7 +31,7 @@ export async function createAuction(req, res) {
       });
     }
 
-    if ((artwork.status !== "DRAFT", "ENDED")) {
+    if (!["DRAFT", "ENDED"].includes(artwork.status)) {
       return res.status(400).json({
         message: "This artwork is already in an auction or has been sold.",
       });
