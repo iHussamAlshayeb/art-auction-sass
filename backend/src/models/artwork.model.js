16e66393c7cc5c61080eb5014353e204582a112c
iHussamAlshayeb <hussamalshayeb01@gmail.com>
2025-10-21 19:26:35 +0300

    Update artwork.model.js

--- backend/src/models/artwork.model.js
+++ backend/src/models/artwork.model.js
@@ -27,8 +27,20 @@ const artworkSchema = new mongoose.Schema(
   },
   {
     timestamps: true,
+    // === 1. تفعيل الحقول الافتراضية ===
+    toJSON: { virtuals: true },
+    toObject: { virtuals: true },
   }
 );
+
+// ---== 2. الحل هنا: إضافة حقل افتراضي (virtual field) ==---
+// هذا يخبر Mongoose بوجود علاقة "عكسية" مع موديل Auction
+artworkSchema.virtual("auction", {
+  ref: "Auction", // الموديل الذي نريد الربط معه
+  localField: "_id", // الحقل الموجود في هذا الموديل (Artwork)
+  foreignField: "artwork", // الحقل المطابق في الموديل الآخر (Auction)
+  justOne: true, // لأنها علاقة واحد لواحد
+});
 
 const Artwork = mongoose.model("Artwork", artworkSchema);
 export default Artwork;
