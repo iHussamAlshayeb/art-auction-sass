5c9b6c77a17371f2cded569f01e0f92c3032191c
iHussamAlshayeb <hussamalshayeb01@gmail.com>
2025-10-27 20:50:12 +0300

    Update rateLimiter.js

--- backend/src/middleware/rateLimiter.js
+++ backend/src/middleware/rateLimiter.js
@@ -2,7 +2,8 @@ import ratelimit from "../config/upstash.js";
 
 const rateLimiter = async (req, res, next) => {
   try {
-    const { success } = await ratelimit.limit("User ID");
+    const userId = req.user?._id || req.body?._id || req.ip;
+    const { success } = await ratelimit.limit(userId);
 
     if (!success) {
       return res.status(429).json({
