363d6e24fd7f6c7c1e8e8433fd0338d30ccc51f1
iHussamAlshayeb <hussamalshayeb01@gmail.com>
2025-10-27 23:28:01 +0300

    Update app.js

--- backend/src/app.js
+++ backend/src/app.js
@@ -19,7 +19,7 @@ const app = express();
 app.use(
   cors({
     origin: [
-      "http://localhost:5173",
+      "http://localhost:5174",
       "https://app.fanan3.com",
       "https://www.fanan3.com",
       "https://fanan3.com",
